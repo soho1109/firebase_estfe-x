1e66e336bff3a1b1bf94db8a00465bdc970096c5
soho1109 <park1109pp@naver.com>
2026-07-09 11:29:56 +0900

    feat: 회원가입 폼 추가

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Box, Typography, TextField, Button } from '@mui/material';
+import { authService } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Auth() {
   const [newAccount, setNewAccount] = useState(true);
@@ -7,6 +9,8 @@ function Auth() {
     email: "",
     password: "",
   });
+
+  const auth = authService;
 
   console.log(form);
   //이메일,비밀번호 변동하면 할일
@@ -18,12 +22,31 @@ function Auth() {
     }));
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (newAccount) {
+      //회원가입
+      createUserWithEmailAndPassword(auth, form.email, form.password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+    } else {
+      //로그인
+    }
+  }
+
   return (
     <>
       <Typography variant="h2" component="h2">
-        Login Form
+        {newAccount ? "회원가입 폼" : "로그인 "}
       </Typography>
-      <Box component="form" sx={{ mt: 2 }}>
+      <Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
         <TextField
           fullWidth
           label="Email address"
@@ -41,7 +64,9 @@ function Auth() {
           variant="outlined"
           onChange={handleChange}
         />
-        <Button sx={{ mt: 2 }} type="sumit" variant="contained">Login</Button>
+        <Button sx={{ mt: 2 }} type="sumit" variant="contained">
+          {newAccount ? "회원가입" : "로그인"}
+        </Button>
       </Box>
     </>
   );
