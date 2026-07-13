85873dce498c2f1e0d38e9dae4384b6c740a30ce
soho1109 <park1109pp@naver.com>
2026-07-13 14:40:12 +0900

    feat: 수정 내용 추가

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,27 +1,76 @@
-import { Divider, ListItem, ListItemText, Button, Stack } from "@mui/material";
+import { Box, TextField, Divider, ListItem, ListItemText, Button, Stack } from "@mui/material";
 import { db } from "../firebase";
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
+import { useState } from "react";
 
-export default function Comment({ item,isShown }) {
-  const handleDelete = async ()=>{
-    if(window.confirm('정말 삭제할까요?')) return;
-    await deleteDoc(doc(db, "comments", item.id));     
-  }
+export default function Comment({ item, isShown }) {
+  const [edit, setEdit] = useState(false);
+  const [comment, setComment] = useState(item.comment);
+
+  const handleDelete = async () => {
+    if (window.confirm("정말 삭제할까요?")) return;
+    await deleteDoc(doc(db, "comments", item.id));
+  };
+  const toggleEditMode = () => {
+    setEdit(prev => !prev);
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const commentRef = doc(db, "comments", item.id);
+
+    // Set the "capital" field of the city 'DC'
+    await updateDoc(commentRef, {
+      comment: comment
+    });
+    setEdit(false);
+  };
+
+  const handleChange = e => {
+    setComment(e.target.value);
+  };
+
   return (
     <ListItem key={item.id} alignItems="flex-center" divider>
-      <ListItemText
-        primary={item.comment}
-        secondary={item.date?.toDate ? item.date.toDate().toLocaleString() : "작성시간 없음"}
-      />
-      {isShown && (
-        <Stack direction="row" spacing={1}>
-          <Button variant="outlined" size="small">
-            수정
-          </Button>
-          <Button variant="contained" color="error" size="small" onClick={handleDelete}>
-            삭제
-          </Button>
-        </Stack>
+      {edit ? (
+        <Box component="form" onSubmit={onSubmit}>
+          <TextField
+            fullWidth
+            label="Comment"
+            placeholder="글을 입력해주세요."
+            type="text"
+            name="comment"
+            variant="outlined"
+            multiline
+            rows={5}
+            value={comment}
+            onChange={handleChange}    
+          />
+          <Stack direction="row" spacing={1} sx={{ mt: 1 }}>
+            <Button sx={{ mt: 2 }} type="submit" variant="contained" size="small">
+              글쓰기
+            </Button>
+            <Button variant="outlined" size="small" onClick={toggleEditMode}>
+              취소
+            </Button>
+          </Stack>
+        </Box>
+      ) : (
+        <>
+          <ListItemText
+            primary={item.comment}
+            secondary={item.date?.toDate ? item.date.toDate().toLocaleString() : "작성시간 없음"}
+          />
+          {isShown && (
+            <Stack direction="row" spacing={1}>
+              <Button variant="outlined" size="small" onClick={toggleEditMode}>
+                수정
+              </Button>
+              <Button variant="contained" color="error" size="small" onClick={handleDelete}>
+                삭제
+              </Button>
+            </Stack>
+          )}
+        </>
       )}
     </ListItem>
   );
